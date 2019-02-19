eaa92f2ffaea5cea660d56ade08accc83ebde6a4
Luiz Ribeiro <luizantonio@gmail.com>
2019-02-19 09:41:28 -0400

    teste tortoisegit

--- teste.php
+++ teste.php
@@ -1,3 +1,4 @@
 v1.0
 v1.1 ainda só no SVN do SERVIDOR
 v1.2 no c:\github\teste\teste.php tendo sido já criado o branch develop e colocado no repositório
+v1.3 alterado no note da Adê com Notepad e TortoiseGit
